4bf7968389b2947f2241e0940cb9d3da81639005
Yesh Chandiramani <yesh0907@hotmail.com>
2016-06-29 20:01:41 +0800

    Updated Models to have resources and author

--- tips/models.py
+++ tips/models.py
@@ -5,8 +5,10 @@ from django.db import models
 # Create your models here.
 class Tip(models.Model):
 	title = models.CharField(max_length=255)
+	author = models.CharField(max_length=255, default="")
+	resources = models.TextField(default="")
 	body = models.TextField()
-	created_at = models.DateTimeField()
+	pub_date = models.DateTimeField()
 
 	def __str__(self):
 		return self.title
