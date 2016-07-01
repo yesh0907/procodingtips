74db5853a0d1f581e9c161269d0b3fb9dd5c3926
Yesh Chandiramani <yesh0907@hotmail.com>
2016-07-02 00:30:02 +0800

    Changed db

--- procodingtips/settings.py
+++ procodingtips/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'procodingtips.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'procodingtips',
+        'USER': 'yesh0907',
+        'PASSWORD': 'steven0907',
+        'HOST': 'locahost',
+        'PORT': '',
     }
 }
 
