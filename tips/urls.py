b847452569524592400179f37b8005b783229cde
Yesh Chandiramani <yesh0907@hotmail.com>
2016-07-04 00:27:43 +0800

    Got likes and dislikes working. Plus added sftp-plugin to my workspace

--- tips/urls.py
+++ tips/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
 	url(r'^series/$', views.all_series_list, name="series"),
 	url(r'^tips/', views.all_tip_list, name="tips"),
 	url(r'^series/(?P<pk>[0-9]+)/', views.all_tips_in_series, name='series_detail'),
+	url(r'^likes/', views.like, name='like'),
+	url(r'^dislikes/', views.dislike, name='dislikes'),
 ]
